3a272c7f129d4bb7a90b437362220e12fe26bdba
aleCombi <alecombi94@gmail.com>
2024-08-31 15:45:03 +0200

    Benchmarking fixed rate flows evaluation symbolically

--- benchmarkCompiledSymbolic.jl
+++ benchmarkCompiledSymbolic.jl
@@ -1,21 +1,40 @@
 using Symbolics
 using BenchmarkTools
+using InteractiveUtils
 
 # Define symbolic variables and create symbolic expression
-@variables x y
-expr = x^2 + 3x + y
+@variables x
+expr = x*x
 
-# Build a callable Julia function from the symbolic expression
-# Convert the returned object to an actual Julia function (if needed)
-f = eval(build_function(expr, [x, y]; type_annotations=[Float64, Float64]))
+# Build a callable Julia function from the symbolic expression with type annotations
+# Modify how the function is built if applicable
+f = build_function(expr, x, expression=Val{false})
 
-function g(x::Float64, y::Float64)::Float64
-    return x^2 + 3x + y
+# Manually defined function with type annotations
+function g(x::Float64)::Float64
+    return x*x
 end
 
-benchmark_f = @benchmark f([2.0, 2.0])
-benchmark_g = @benchmark g(2.0, 2.0)
+# Benchmarking both functions
+benchmark_f = @benchmark f(2.0)
+benchmark_g = @benchmark g(2.0)
 
-# Print the results
-println("Benchmarking for f(2,2): ", benchmark_f)
-println("Benchmarking for g(2,2): ", benchmark_g)
+# Print benchmark results
+println("Benchmarking for f(2.0, 2.0) with type annotations: ")
+println(benchmark_f)
+
+println("\nBenchmarking for g(2.0, 2.0): ")
+println(benchmark_g)
+
+# # Introspection using @code_llvm and @code_native
+# println("\nCompiled LLVM code for f([2.0, 2.0]) with type annotations:")
+# @code_llvm f(2.0, 2.0)
+
+# println("\nCompiled LLVM code for g(2.0, 2.0):")
+# @code_llvm g(2.0, 2.0)
+
+# println("\nNative assembly code for f([2.0, 2.0]) with type annotations:")
+# @code_native f(2.0, 2.0)
+
+# println("\nNative assembly code for g(2.0, 2.0):")
+# @code_native g(2.0, 2.0)
