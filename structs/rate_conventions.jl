5a5a2445f7f0b63c923dc0c0418c3fcaec1ddc3b
Combi, A (Alessandro) <alessandro.combi@rabobank.com>
2024-09-15 11:25:09 +0200

    Linear and COmpounded rate types renamed

--- structs/rate_conventions.jl
+++ structs/rate_conventions.jl
@@ -3,7 +3,7 @@ module RateConventions
 using Types
 
 # Export the abstract type and concrete rate types
-export RateType, LIN, COM, calculate_interest
+export RateType, Linear, Compounded, calculate_interest
 
 # Define an abstract type for rate types
 """
@@ -14,33 +14,34 @@ Abstract type representing a rate type. This serves as the base type for all spe
 abstract type RateType end
 
 """
-    LIN <: RateType
+    Linear <: RateType
 
 Concrete type representing linear (simple) interest, where the interest is calculated as a fixed percentage of the principal over time.
 """
-struct LIN <: RateType end
+struct Linear <: RateType end
 
 """
-    COM <: RateType
+    Compounded <: RateType
 
 Concrete type representing compound interest, where the interest is added to the principal after each period, and interest is calculated on the new balance.
 """
-struct COM <: RateType end
+struct Compounded <: RateType end
 
 """
     calculate_interest(principal::Float64, rate::CalcValue, time_fraction::Float64, ::LIN) -> Float64
 
 Calculates interest using the linear (simple) interest method. The interest is calculated as a percentage of the principal for a given time period.
+Rate and return type are Float64 or symbolic expression.
 
 # Arguments
 - `principal::Float64`: The initial amount of money (the principal).
-- `rate::CalcValue`: The interest rate to be applied.
+- `rate`: The interest rate to be applied.
 - `time_fraction::Float64`: The fraction of the year the principal is invested for (e.g., 0.5 for 6 months).
 
 # Returns
-- `Float64`: The calculated simple interest.
+- The calculated simple interest.
 """
-function calculate_interest(principal::Float64, rate::CalcValue, time_fraction::Float64, ::LIN)
+function calculate_interest(principal::Float64, rate, time_fraction::Float64, ::Linear)
     return principal * rate * time_fraction
 end
 
@@ -48,17 +49,18 @@ end
     calculate_interest(principal::Float64, rate::CalcValue, time_fraction::Float64, frequency::Int, ::COM) -> Float64
 
 Calculates interest using the compound interest method. The interest is calculated on the principal and accumulated interest over multiple periods.
+Rate and return type are Float64 or symbolic expression.
 
 # Arguments
 - `principal::Float64`: The initial amount of money (the principal).
-- `rate::CalcValue`: The interest rate to be applied.
+- `rate`: The interest rate to be applied.
 - `time_fraction::Float64`: The fraction of the year the principal is invested for (e.g., 0.5 for 6 months).
 - `frequency::Int`: The number of compounding periods per year (e.g., 12 for monthly compounding).
 
 # Returns
-- `Float64`: The calculated compound interest.
+- The calculated compound interest.
 """
-function calculate_interest(principal::Float64, rate::CalcValue, time_fraction::Float64, frequency::Int, ::COM)
+function calculate_interest(principal::Float64, rate, time_fraction::Float64, frequency::Int, ::Compounded)
     return principal * (1 + rate / frequency)^(frequency * time_fraction) - principal
 end
 
--- structs/types.jl
+++
@@ -1,9 +0,0 @@
-module Types
-
-# types.jl or common.jl
-using Symbolics
-
-# Alias that supports both scalars and arrays of Real and Num
-const CalcValue = Union{Real, Num, AbstractArray{<:Real}, AbstractArray{<:Num}}
-
-end
