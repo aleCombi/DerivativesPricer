aaaa60db4a378660aa1430f1e97211f45712e037
aleCombi <alecombi94@gmail.com>
2024-10-08 20:21:50 +0200

    Including test files in runtest only in vs code for code completion

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 using TestItemRunner
 using DerivativesPricer
 
-include("day_count_conventions.jl")
-include("rate_conventions.jl")
-include("schedule_generation.jl")
-include("fixed_rate_stream.jl")
-include("rate_curves.jl")
-include("float_rate_stream.jl")
-include("discount_pricing.jl")
+if isdefined(@__MODULE__,:LanguageServer)
+    include("day_count_conventions.jl")
+    include("rate_conventions.jl")
+    include("schedule_generation.jl")
+    include("fixed_rate_stream.jl")
+    include("rate_curves.jl")
+    include("float_rate_stream.jl")
+    include("discount_pricing.jl")
     
 @run_package_tests
