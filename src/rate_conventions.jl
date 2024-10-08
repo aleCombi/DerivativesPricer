981eb1be999682781c66767016e6991ed8585a12
aleCombi <alecombi94@gmail.com>
2024-10-08 19:43:17 +0200

    simplified functions to calculate interest

--- src/rate_conventions.jl
+++ src/rate_conventions.jl
@@ -32,23 +32,6 @@ Concrete type representing exponential interest, where the interest is calculate
 struct Exponential <: RateType end
 
 """
-    calculate_interest(principal::Float64, rate::CalcValue, time_fraction::Float64, ::Linear) -> Float64
-
-Calculates interest using the linear (simple) interest method. The interest is calculated as a fixed percentage of the principal over a given time period.
-
-# Arguments
-- `principal::Float64`: The initial amount of money (the principal).
-- `rate::CalcValue`: The interest rate to be applied, where `CalcValue` can be either `Float64` or a symbolic expression.
-- `time_fraction::Float64`: The fraction of the year the principal is invested for (e.g., 0.5 for 6 months).
-
-# Returns
-- `Float64`: The calculated simple interest.
-"""
-function calculate_interest(principal, rate, time_fraction, ::Linear)
-    return principal * rate * time_fraction
-end
-
-"""
     calculate_interest(principals::Vector{Float64}, rates::Vector{Float64}, time_fractions::Vector{Float64}, ::Linear) -> Vector{Float64}
 
 Calculates interest for multiple principals using the linear (simple) interest method. This vectorized version handles multiple investments.
@@ -61,26 +44,8 @@ Calculates interest for multiple principals using the linear (simple) interest m
 # Returns
 - `Vector{Float64}`: A vector of calculated simple interest for each investment.
 """
-function calculate_interest(principals::Vector{T}, rates::Vector{S}, time_fractions::Vector{U}, ::Linear) where {T,S,U}
-    return principals .* rates .* time_fractions
-end
-
-"""
-    calculate_interest(principal::Float64, rate::CalcValue, time_fraction::Float64, rate_type::Compounded) -> Float64
-
-Calculates interest using the compound interest method. Interest is compounded over multiple periods, and the interest is calculated on the principal and accumulated interest.
-
-# Arguments
-- `principal::Float64`: The initial amount of money (the principal).
-- `rate::CalcValue`: The interest rate to be applied, where `CalcValue` can be either `Float64` or a symbolic expression.
-- `time_fraction::Float64`: The fraction of the year the principal is invested for (e.g., 0.5 for 6 months).
-- `rate_type::Compounded`: An instance of `Compounded` specifying the frequency of compounding.
-
-# Returns
-- `Float64`: The calculated compound interest.
-"""
-function calculate_interest(principal, rate, time_fraction, rate_type::Compounded)
-    return principal * (1 + rate / rate_type.frequency)^(rate_type.frequency * time_fraction) - principal
+function calculate_interest(principal, rate, time_fraction, ::Linear)
+    return principal .* rate .* time_fraction
 end
 
 """
@@ -97,25 +62,8 @@ Calculates compound interest for multiple principals. This vectorized version ha
 # Returns
 - `Vector{Float64}`: A vector of calculated compound interest for each investment.
 """
-function calculate_interest(principals::Vector{P}, rates::Vector{R}, time_fractions::Vector{T}, rate_type::Compounded) where {P,R,T}
-    return principals .* ((1 .+ rates ./ rate_type.frequency) .^ (rate_type.frequency .* time_fractions)) .- principals
-end
-
-"""
-    calculate_interest(principal::Float64, rate::CalcValue, time_fraction::Float64, ::Exponential) -> Float64
-
-Calculates interest using the exponential interest method. The interest is calculated as the principal multiplied by the exponential of the interest rate times the time fraction.
-
-# Arguments
-- `principal::Float64`: The initial amount of money (the principal).
-- `rate::CalcValue`: The interest rate to be applied, where `CalcValue` can be either `Float64` or a symbolic expression.
-- `time_fraction::Float64`: The fraction of the year the principal is invested for (e.g., 0.5 for 6 months).
-
-# Returns
-- `Float64`: The calculated exponential interest.
-"""
-function calculate_interest(principal, rate, time_fraction, ::Exponential)
-    return principal * exp(rate * time_fraction) - principal
+function calculate_interest(principal, rate, time_fraction, rate_type::Compounded)
+    return principal .* ((1 .+ rate ./ rate_type.frequency) .^ (rate_type.frequency .* time_fraction)) .- principal
 end
 
 """
@@ -131,6 +79,6 @@ Calculates exponential interest for multiple principals. This vectorized version
 # Returns
 - `Vector{Float64}`: A vector of calculated exponential interest for each investment.
 """
-function calculate_interest(principals::Vector{P}, rates::Vector{R}, time_fractions::Vector{T}, ::Exponential) where {P,R,T}
-    return principals .* (exp.(rates .* time_fractions)) .- principals
+function calculate_interest(principal, rate, time_fraction, ::Exponential)
+    return principal .* (exp.(rate .* time_fraction)) .- principal
 end
