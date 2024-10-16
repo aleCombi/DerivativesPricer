eab52f840d061cc624947b50b4506345568cf04a
aleCombi <aleCombi94@gmail.com>
2024-10-16 17:23:24 +0200

    Moved schedule code its own folder, started working on schedules with stubs, adjustments and rolls

---
+++ src/date_generation/stub_period.jl
@@ -0,0 +1,14 @@
+struct StubPeriod{P<:StubPosition, L<:StubLength}
+    position::P
+    length::L
+end
+
+abstract type StubPosition end
+
+struct UpfrontStubPosition <: StubPosition end
+struct InArrearsStubPosition <: StubPosition end
+
+abstract type StubLength end
+
+struct ShortStubLength <: StubLength end
+struct LongStubLength <: StubLength end
