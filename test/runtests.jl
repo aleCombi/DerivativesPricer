17dacfe3178e47a3715a4ed3b1b414bb3bc08eff
aleCombi <alecombi94@gmail.com>
2024-10-27 12:52:14 +0100

    Moved day_count_conventions unit tests to test/unit

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using DerivativesPricer #TODO:explore the use of @testable to avoid exporting ev
 
 # include test files in VSCODE: necessary to have code completion
 if isdefined(@__MODULE__,:LanguageServer)
-    include("day_count_conventions.jl")
+    include("unit/day_count_conventions.jl")
     include("rate_conventions.jl")
     include("date_generation/include_date_generation.jl")
     include("integration_tests/include_integration_tests.jl")
