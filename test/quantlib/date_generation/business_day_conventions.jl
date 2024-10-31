7c332134dca1155dc1cf51f977aa5545dd2bb771
aleCombi <alecombi94@gmail.com>
2024-11-01 00:07:22 +0100

    Completing test setup

--- test/quantlib/date_generation/business_day_conventions.jl
+++ test/quantlib/date_generation/business_day_conventions.jl
@@ -15,7 +15,20 @@
     ql_calendar = ql.WeekendsOnly()
     ql_calendar.addHoliday(ql.Date(1,1,2023))
     ql_calendar.addHoliday(ql.Date(25,12,2023))
-    include("../quantlib_helpers.jl")
+
+    function ql_to_julia_date(ql_date)
+        # Extract year, month, and day from the QuantLib.Date object
+        year = Int(ql_date.year())
+        month = Int(ql_date.month())
+        day = Int(ql_date.dayOfMonth())
+        
+        # Construct and return the Julia Date
+        return Date(year, month, day)
+    end
+
+    function julia_to_ql_date(julia_date)
+        return ql.Date(day(julia_date), month(julia_date), year(julia_date))
+    end
 end
 
 @testitem "Quantlib PreviousBusinessDay" setup=[QuantlibBusinessDayConvention] begin
