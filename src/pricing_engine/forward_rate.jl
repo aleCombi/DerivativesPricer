104689598431f1a4320b3921b3403b25a816dd6e
aleCombi <alecombi94@gmail.com>
2024-11-12 12:32:45 +0100

    broadcaster -

--- src/pricing_engine/forward_rate.jl
+++ src/pricing_engine/forward_rate.jl
@@ -177,7 +177,7 @@ Calculates the compounded interest accruals over each period in `simple_schedule
 function period_compounded_accrual(simple_schedule::SimpleRateStreamSchedules, rate_curve::R, rate_type::T, margin_config::MarginOnUnderlying) where {R<:AbstractRateCurve, T<:RateType}
     forwards = forward_rate(simple_schedule, rate_curve, rate_type, margin_config.margin_config)
     compounding_factors = compounding_factor(forwards, simple_schedule.accrual_day_counts, rate_type)
-    compound_after_i = vcat([prod(compounding_factors[i+1:end]) for i in 1:(length(compounding_factors)-1)],1)
+    compound_after_i = vcat([prod(compounding_factors[i+1:end]) for i in 1:(length(compounding_factors).-1)],1)
     sub_addend = simple_schedule.accrual_day_counts .* forwards
     return 1 + sum(sub_addend .* compound_after_i)
 end
